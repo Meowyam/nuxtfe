f44adb7111a4e4964299db60ff8ef8bed0d7f89a
Meowyam <maryam@chilon.net>
2021-09-12 00:27:03 +0800

    moved selector to selector

--- components/Form.vue
+++ components/Form.vue
@@ -1,13 +1,8 @@
 <template>
     <body>
         <form>
-            <div v-for="(value, name, index) in options">
-                <v-select
-                :options="value"
-                @input="name => updatePlayer(name,index)">
-                </v-select>
-            </div>
-
+            <selector :options="options" :things="things" :mySet="mySet" :arr="arr" :n="n">
+            </selector>
         </form>
         meow
         <div>
@@ -26,41 +21,16 @@
         data() {
             return {
             options,
-            arr: [],
             things: null,
-            mySet: null
-            }
-        },
-        methods: {
-            updatePlayer(x,i) {
-                console.log(x,i)
-                let a = this.things[i]
-                let b = ("entry" + i)
-                b = new Object()
-                b[a] = x
-                let c = b[a]
-                console.log(b)
-                console.log(a, b, c)
-                //let c = a[b]
-                this.arr.push(c)
-                //console.log(a[b])
-                this.checkIfEnd(x,i)
-            },
-            checkIfEnd(x,i) {
-                console.log((i+1) == (Object.keys(this.options).length))
-                // if ((i) == Object.keys(this.options).length) {
-                //     mySet.add(c)
-                //     this.arr = []
-                //     console.log(mySet)
-                // } else {
-                //     this.updatePlayer(x,i)
-                // }
+            mySet: null,
+            arr: [],
+            n: 1,
             }
         },
         mounted() {
             this.things = Object.keys(options)
             this.mySet = new Set()
-            console.log(this.things)
+            console.log((Object.values(this.options)[0]).length)
         }
     }
 </script>
---
+++ components/Selector.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div v-for="(value, name, index) in options">
+            <v-select
+            :options="value"
+            @input="name => updatePlayer(name, index, n)">
+            </v-select>
+            <!-- <v-select
+            :options="value"
+            @input="name => updatePlayer(name,index,n)">
+            </v-select> -->
+        </div>
+    </div>
+</template>
+<!--(value, name, index)-->
+
+<script>
+export default{
+    name: 'selector',
+    props: {
+        options: Object,
+        things : Array,
+        mySet  : Set,
+        arr    : Array,
+        n      : Number
+    },
+    methods: {
+        updatePlayer(x, i, n) {
+            console.log(this.things)
+            console.log(x)
+            console.log(i)
+            this.addSelect = false
+            let a = this.things[i]
+            let b = ("entry" + i)
+            b = new Object()
+            b[a] = x
+            let c = b[a]
+            console.log(b)
+            console.log(a, b, c)
+            console.log(this.arr)
+            this.arr.push(c)
+            console.log(c)
+            console.log(n)
+            //this.checkIfEnd(x,c,i,n)
+        },
+        checkIfEnd(x,c,i,n) {
+            console.log(c)
+            if ((i+1) == Object.keys(this.options).length) {
+                this.mySet.add(this.arr)
+                console.log(this.arr)
+                console.log(this.mySet)
+            } else {
+                // this.updatePlayer(x,c,i)
+            }
+        }
+    },
+}
+</script>
+
